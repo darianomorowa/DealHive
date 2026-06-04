f506d5835d593a2d730b6840d0238b0432065cdf
gittyDanny <daniilioffe2000@gmail.com>
2026-06-04 11:15:09 +0200

    hives werden nun bei /hives angezeigt

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from database import create_tables, get_all_hives
 
 app = Flask(__name__)
 
@@ -7,11 +8,27 @@ app = Flask(__name__)
 def home():
     return "meine erste kleine Seite hehehe"
 
-
 @app.route("/hives")
 def hives_overview():
-    return "hier werden hoffentlich bald Hives angezeigt!!!"
+    # hier stellen wir sicher, dass die Tabelle existiert
+    create_tables()
 
+    # hier holen wir die Hives aus der Datenbank
+    hives = get_all_hives()
+
+    # erstmal simples HTML direkt hier, damit die Logik sichtbar bleibt
+    html = "<h1>Hives entdecken</h1>"
+
+    for hive in hives:
+        html += f"""
+        <div>
+            <h2>{hive["title"]}</h2>
+            <p>ID: {hive["id"]}</p>
+        </div>
+        <hr>
+        """
+
+    return html
 
 if __name__ == "__main__":
     app.run(debug=True)
