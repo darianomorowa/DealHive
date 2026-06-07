af1d27e561c1edc11bcbc2a60ecb03c511648532
gittyDanny <daniilioffe2000@gmail.com>
2026-06-07 10:48:40 +0200

    Jetzt werden auch die Deadlines unt Teilnehmer als Details angezeigt

--- app.py
+++ app.py
@@ -22,17 +22,22 @@ def hives_overview():
     # Schleife für alle Hives mit Detaillink für jeden Hive
 
     for hive in hives:
+    # hier bauen wir für jeden Hive einen kleinen HTML-Block
+    # Deadline und Teilnehmerstand sind wichtig, weil Käufer das direkt sehen sollen
         html += f"""
         <div>
-            <h2>{hive["title"]}</h2>
-            <p>ID: {hive["id"]}</p>
-            <a href="/hives/{hive["id"]}">Details ansehen</a>
+        <h2>{hive["title"]}</h2>
+        <p>Deadline: {hive["deadline"]}</p>
+        <p>Teilnehmer: {hive["current_participants"]} / {hive["min_participants"]}</p>
+        <a href="/hives/{hive["id"]}">Details ansehen</a>
         </div>
         <hr>
         """
 
     return html
 
+
+#Detailroute
 @app.route("/hives/<int:hive_id>")
 def hive_detail(hive_id):
     # hier holen wir genau den einen =ID Hive aus der Datenbank
@@ -45,7 +50,11 @@ def hive_detail(hive_id):
     
     return f"""
     <h1>{hive["title"]}</h1>
+
     <p>ID: {hive["id"]}</p>
+    <p>Deadline: {hive["deadline"]}</p>
+    <p>Teilnehmer: {hive["current_participants"]} / {hive["min_participants"]}</p>
+
     <a href="/hives">Zurück zu allen Hives</a>
     """
 
